3e6fdead76e6aed9d91405fe0e2b8391521a31e4
Guo-Linghui <guolh2013@hotmail.com>
2019-05-28 01:02:46 +0800

    Slight modifications and I'll rest.

--- tools/lsimg.py
+++ tools/lsimg.py
@@ -6,6 +6,7 @@ allFiles = os.listdir(sys.argv[1])
 allFiles.sort()
 
 allFiles = [ "project/" + sys.argv[1] + "/" +  a for a in allFiles ]
+allFiles = ["project/img_cover.png"] + allFiles
 listStr = "var picArray = " + str(allFiles);
 
 f = open("project/src/piclist.js", "w+")
