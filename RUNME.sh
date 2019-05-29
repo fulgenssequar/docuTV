58901a703fb6de8d83ac90e5f140f2053bc4d153
Guo-Linghui <guolh2013@hotmail.com>
2019-05-29 20:14:24 +0800

    Error Fixed

--- RUNME.sh
+++ RUNME.sh
@@ -1,6 +1,6 @@
 #: /usr/bin
 
-serverPort="8888"
+serverPort="13531"
 imagedir="./project/resources/imgdoc/"
 bookdir="./project/resources/doc/"
 
@@ -8,6 +8,7 @@ echo "Have you placed all files into $(realpath $bookdir) properly?"
 cd tools;
 python3 lspdf.py $bookdir || echo " ! You must have  python3 in your  \$PATH and  allow it  to run in this program...  "
 python3 lsimg.py $imagedir || echo " ! You are suggested to place some images into $imagedir as the pages of a default document ...  "
+cd ..
 
 echo ;
 echo "Try opening your browser and visiting:  http://localhost:$serverPort"
